67d066209b868244308decd60ae76bac0df6aa3c
TinySmash <islam.gueniari@gmail.com>
2024-04-25 00:55:32 +0100

    first commit (project setup)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#0135F0",
+        secondary: "#5073F0",
+        third: "#6700F0",
+        black: "#040611",
+        white: "#F2F2F2",
       },
     },
   },
