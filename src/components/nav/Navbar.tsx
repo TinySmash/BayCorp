68f3f8c2b0ab94865a361ec0dec407e4409342b1
TinySmash <islam.gueniari@gmail.com>
2024-06-03 21:12:47 +0100

    nav color on scroll

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -4,13 +4,34 @@ import Link from "next/link";
 import Panel from "./Panel";
 import { useDispatch } from "react-redux";
 import { toggleNav } from "@/redux/reducers/navSlice";
+import { useEffect, useRef } from "react";
 
 function Navbar() {
   const dispatch = useDispatch();
+  const navRef = useRef<any>(null);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 200) {
+        navRef.current?.classList?.replace("bg-transparent", "bg-black");
+        // navRef.current?.classList.add("bg-opacity-75")
+      } else {
+        navRef.current?.classList?.replace("bg-black", "bg-transparent");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <>
-      <header className="fixed sec-container xl:left-1/2 xl:-translate-x-1/2  w-full h-auto px-5 md:px-10 xl:px-16 py-4 md:py-6 xl:py-8 flex justify-between items-center z-[999]">
+      <header
+        className="fixed bg-transparent sec-container xl:left-1/2 xl:-translate-x-1/2  w-full h-auto px-5 md:px-10 xl:px-16 py-4 md:py-6 xl:py-8 flex justify-between items-center z-[999] bg-opacity-85"
+        ref={navRef}
+      >
         <h1 className="font-bold text-xl md:text-2xl xl:text-3xl text-primary">
           Bay<span className="text-third">Corp</span>
         </h1>
